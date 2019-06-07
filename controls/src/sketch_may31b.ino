c3e62b892f4d8080df3401099217c25f1c206c8b
Kyle <43458322+kyle-stevens@users.noreply.github.com>
2019-06-07 12:31:03 -0700

    serial
    memory optimization, reduced usage b 8 btes

--- controls/src/sketch_may31b.ino
+++ controls/src/sketch_may31b.ino
@@ -1,19 +1,10 @@
 #include "ros.h"
-
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
 #include "std_msgs/UInt16MultiArray.h"
-
-
-
 unsigned int motorVals[8];
 unsigned int* arrayIter;
-
 ros::NodeHandle nh;
-
-
-
-
 void arrayCallback(const std_msgs::UInt16MultiArray& motorMSGHead)
 {
   int i;
@@ -23,18 +14,14 @@ void arrayCallback(const std_msgs::UInt16MultiArray& motorMSGHead)
     motorVals[i] = *arrayIter;
     arrayIter++;
   }
-  Serial.println("Values Received\n");
-
+  Serial.println("Values \n");
 }
-
 ros::Subscriber<std_msgs::UInt16MultiArray> sub("array", arrayCallback);
-
 void setup() {
   nh.initNode();
   nh.subscribe(sub);
   Serial.begin(57600);
 }
-
 void loop() {
   nh.spinOnce();
   delay(1);
