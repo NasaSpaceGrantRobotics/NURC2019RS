de7abdefe0386fccff27bd9c2eebf97eb81093f1
Cordell Michaud <10409047+michaudcordell@users.noreply.github.com>
2019-06-21 00:17:34 -0700

    Complete basic functionality of claw handler
    - Add subscription to claw target transaltion and rotation topic
    - Add PWM scaling to messages received from claw target translation and rotation  topic
    - Add robot-side emergency stop functionality for claw control

--- controls/src/claw_handler.py
+++ controls/src/claw_handler.py
@@ -1,1 +1,76 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
+
+
+class ClawHandler:
+
+    def __init__(self):
+        self.__e_stop = False
+        self.claw_values_pub = rospy.Publisher('claw_values', Twist, queue_size=10)
+        self.__trans_scaled_bounds = (0, 1700)
+        self.__rot_scaled_bounds = (0, 1700)
+        self.claw_values_msg = Float32MultiArray()
+        self.claw_values_msg.data = [self.scale_trans_normed_to_pwm(0.0), self.scale_rot_normed_to_pwm(0.0)]
+
+
+    @staticmethod
+    def __bound_number(number, bounds):
+        if number < bounds[0]:
+            number = bounds[0]
+        elif number > bounds[1]:
+            number = bounds[1]
+
+        return number
+
+    def scale_trans_normed_to_pwm(self, trans_normed):
+        minimum = self.__trans_scaled_bounds[0]
+        maximum = self.__trans_scaled_bounds[1]
+
+        return self.__bound_number(int(minimum + ((trans_normed + 1.0) / 2.0) * (maximum - minimum)),
+                                   [minimum, maximum])
+
+    def scale_rot_normed_to_pwm(self, rot_normed):
+        minimum = self.__rot_scaled_bounds[0]
+        maximum = self.__rot_scaled_bounds[1]
+
+        return self.__bound_number(int(minimum + ((rot_normed + 1.0) / 2.0) * (maximum - minimum)),
+                                   [minimum, maximum])
+
+    def claw_callback(self, msg):
+        claw_trans_normed = msg.linear.x
+        claw_rot_normed = msg.angular.y
+
+        if self.__e_stop:
+            claw_trans_normed = 0.0
+            claw_rot_normed = 0.0
+
+        claw_trans_scaled = self.scale_trans_normed_to_pwm(claw_trans_normed)
+        claw_rot_scaled = self.scale_rot_normed_to_pwm(claw_rot_normed)
+
+        self.claw_values_msg.data = [self.scale_trans_normed_to_pwm(claw_trans_scaled),
+                                     self.scale_rot_normed_to_pwm(claw_rot_scaled)]
+
+        self.claw_values_pub.publish(self.claw_values_msg)
+
+    def e_stop_callback(self, msg):
+        self.__e_stop = msg.data
+
+    def start(self):
+        rospy.Subscriber('target_claw', Twist, self.claw_callback)
+        rospy.Subscriber('e_stop', Bool, self.e_stop_callback)
+
+        rospy.init_node('input_proc')
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    try:
+        input_proc = InputProcessor()
+        input_proc.start()
+    except rospy.ROSInterruptException:
+        pass
+
