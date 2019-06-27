6def4128cf35907da85d24a65e82e23506dc1d85
Cordell Michaud <10409047+michaudcordell@users.noreply.github.com>
2019-06-27 16:11:33 -0700

    Prepare for basic Arduino functionality

--- controls/src/sketch_may31b.ino
+++ controls/src/sketch_may31b.ino
@@ -1,27 +1,60 @@
 #include "ros.h"
 #include "std_msgs/MultiArrayLayout.h"
 #include "std_msgs/MultiArrayDimension.h"
-#include "std_msgs/UInt16MultiArray.h"
-unsigned int motorVals[8];
-unsigned int* arrayIter;
+#include "std_msgs/Int32MultiArray.h"
+#include "std_msgs/Bool.h"
+
+int driveVals[8];
+int clawVals[2];
+int* arrayIter;
 ros::NodeHandle nh;
-void arrayCallback(const std_msgs::UInt16MultiArray& motorMSGHead)
+
+void driveCallback(const std_msgs::Int32MultiArray& motorMSGHead)
 {
   int i;
   arrayIter = motorMSGHead.data;
   for (i = 0; i < 8; i++)
   {
-    motorVals[i] = *arrayIter;
+    driveVals[i] = *arrayIter;
     arrayIter++;
   }
-  Serial.println("Values \n");
 }
-ros::Subscriber<std_msgs::UInt16MultiArray> sub("array", arrayCallback);
+
+void clawCallback(const std_msgs::Int32MultiArray& motorMSGHead)
+{
+  int i;
+  arrayIter = motorMSGHead.data;
+  for (i = 0; i < 2; i++)
+  {
+    clawVals[i] = *arrayIter;
+    arrayIter++;
+  }
+}
+
+void lightCallback(const std_msgs::Bool& desiredLightState)
+{
+    bool lightState = *desiredLightState.data;
+}
+
+void eStopCallback(const std_msgs::Bool& desiredEStopState)
+{
+    bool eStopState = *desiredEStopState.data;
+}
+
+ros::Subscriber<std_msgs::Int32MultiArray> driveSub("drive_values", driveCallback);
+ros::Subscriber<std_msgs::Int32MultiArray> clawSub("claw_values", clawCallback);
+ros::Subscriber<std_msgs::Bool> lightSub("light", lightCallback);
+ros::Subscriber<std_msgs::Bool> eStopSub("e_stop", eStopCallback);
+
 void setup() {
   nh.initNode();
-  nh.subscribe(sub);
+  nh.subscribe(driveSub);
+  nh.subscribe(clawSub);
+  nh.subscribe(lightSub);
+  nh.subscribe(eStopSub);
   Serial.begin(57600);
 }
+
 void loop() {
   nh.spinOnce();
   delay(1);
